79e11ced272c6ed7a6b9e61c93212c8bf076c435
uwen_godwin <uwengodwin001@gmail.com>
2024-06-26 14:18:17 +0100

    first commit

--- 0x00-ES6_basic/12-main.js
+++ 0x00-ES6_basic/12-main.js
@@ -1,4 +1,11 @@
 import createEmployeesObject from './11-createEmployeesObject.js';
 import createReportObject from './12-createReportObject.js';
 
-const
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
+};      
+
+const report = createReportObject(employees);
+console.log(report.allEmployees);
+console.log(report.getNumberOfDepartments(report.allEmployees));

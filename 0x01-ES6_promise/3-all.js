6b77537fa845b4f809a45ac4f4919ca4abd12cb5
uwen_godwin <uwengodwin001@gmail.com>
2024-06-27 05:45:15 +0100

    first commit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,9 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
+const p = uploadPhoto();
+const newUser = createUser();
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((responses) => {
-      const [photo, user] = responses;
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
+  Promise.all([p, newUser])
+    .then((values) => console.log(values[0].body, values[1].firstName, values[1].lastName))
     .catch(() => console.log('Signup system offline'));
 }

52acac34a340707c3c61401a4a0032b259e64729
uwen_godwin <uwengodwin001@gmail.com>
2024-06-27 16:58:54 +0100

    handleResponseFromAP

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,16 +2,20 @@ import { uploadPhoto, createUser } from './utils.js';
 
 async function asyncUploadUser() {
   try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+
+    const [photoResponse, userResponse] = await Promise.all([photoPromise, userPromise]);
+
     return {
-      photo,
-      user,
+      photo: photoResponse,
+      user: userResponse
     };
   } catch (error) {
+    console.error("Error occurred during async operations:", error);
     return {
       photo: null,
-      user: null,
+      user: null
     };
   }
 }

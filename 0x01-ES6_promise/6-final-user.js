115a4d43d45d38ca3ec6052c25315581586d1151
uwen_godwin <uwengodwin001@gmail.com>
2024-06-27 05:55:28 +0100

    first commit

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,13 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.value ? result.value : result.reason,
+    })));
 }

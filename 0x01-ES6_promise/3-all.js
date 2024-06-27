1fb0c1ff94819c82452cc9da9355d17708aa45a2
uwen_godwin <uwengodwin001@gmail.com>
2024-06-27 16:30:07 +0100

    handleResponseFromAP

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
-const p = uploadPhoto();
-const newUser = createUser();
-export default function handleProfileSignup() {
-  Promise.all([p, newUser])
-    .then((values) => console.log(values[0].body, values[1].firstName, values[1].lastName))
-    .catch(() => console.log('Signup system offline'));
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
+
+export default handleProfileSignup;

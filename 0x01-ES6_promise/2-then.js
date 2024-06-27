a7b1470685d0360f79a114e9c28f937bca010544
uwen_godwin <uwengodwin001@gmail.com>
2024-06-27 16:21:52 +0100

    handleResponseFromAP

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,17 @@
-export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
+ function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      return new Error();
+    })
+    .finally(() => {
       console.log('Got a response from the API');
-    } else {
-      reject(new Error());
-    }
-  });
+    });
 }
+
+export default handleResponseFromAPI;

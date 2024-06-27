1a6735cc4d019e6d389811eec357aa411aff067e
uwen_godwin <uwengodwin001@gmail.com>
2024-06-27 06:07:00 +0100

    first commit

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'success' });
       console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+    } else {
+      reject(new Error());
+    }
+  });
 }

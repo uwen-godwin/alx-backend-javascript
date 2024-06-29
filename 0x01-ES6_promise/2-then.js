2d333b10b88ca2f2c6d1bfbc765c9194db945bcf
uwen_godwin <uwengodwin001@gmail.com>
2024-06-29 13:41:16 +0100

    ES6 Basics

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
-      console.log('Got a response from the API');
-    } else {
-      reject(new Error());
-    }
+      if (promise) {
+          resolve({ status: 200, body: 'success' });
+          console.log('Got a response from the API');
+      } else {
+          reject(new Error());
+      }
   });
 }

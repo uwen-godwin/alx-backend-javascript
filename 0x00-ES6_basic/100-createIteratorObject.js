9ae7074dc695bcaf714ce22404ea9a10cc536841
uwen_godwin <uwengodwin001@gmail.com>
2024-06-29 13:19:44 +0100

    ES6 Basics

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,16 +1,13 @@
 export default function createIteratorObject(report) {
   const employees = [];
 
-  // Iterate through each department in the report
   for (const department in report) {
       if (Object.prototype.hasOwnProperty.call(report, department)) {
-          // Iterate through each employee in the current department
-          report[department].forEach(employee => {
+          for (const employee of report[department]) {
               employees.push(employee);
-          });
+          }
       }
   }
 
-  // Return an iterator over the employees array
   return employees[Symbol.iterator]();
 }

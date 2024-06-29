8f86bc299609146dd6078bb19ddf0a70bce1a14f
uwen_godwin <uwengodwin001@gmail.com>
2024-06-29 14:30:26 +0100

    ES6 Promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,14 @@
-// Import the required functions
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-// Implement and export the handleProfileSignup function
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Call the signUpUser and uploadPhoto functions with the provided arguments
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  // Use Promise.allSettled to wait for both promises to settle and return their results
-  return Promise.allSettled([userPromise, photoPromise])
-    .then(results => results.map(result => ({
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => 
+    results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+    }))
+  );
 }

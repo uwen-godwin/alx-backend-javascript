b100e45cb3c947ecba0b629532405df6cf0b2226
uwen_godwin <uwengodwin001@gmail.com>
2024-06-29 14:08:34 +0100

    ES6 Basics

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,12 @@
+/**
+ * Appends handlers to the given Promise.
+ * @param {Promise<any>} promise - The Promise to handle.
+ * @returns {Promise<Object>} - Object with status and body attributes on resolve,
+ *                              Empty Error object on reject.
+ */
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-      if (promise) {
-          resolve({ status: 200, body: 'success' });
-          console.log('Got a response from the API');
-      } else {
-          reject(new Error());
-      }
-  });
+  return promise.then(() => {
+    console.log("Got a response from the API");
+    return { status: 200, body: 'success' };
+  }).catch(() => new Error());
 }

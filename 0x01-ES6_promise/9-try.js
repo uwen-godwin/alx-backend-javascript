1eae4047f9c73898602abf3ec94cf2282dc7e404
uwen_godwin <uwengodwin001@gmail.com>
2024-06-29 14:37:32 +0100

    ES6 Promise

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,11 @@
+
 export default function guardrail(mathFunction) {
   const queue = [];
-  
   try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(`Error: ${error.message}`);
-  } finally {
-    queue.push('Guardrail was processed');
+    queue.push(mathFunction());
+  } catch (e) {
+    queue.push(e);
   }
-
+  queue.push('Guardrail was processed');
   return queue;
 }

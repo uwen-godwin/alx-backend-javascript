4ab75559c06b857774e48a13dd9aa5f4adc80598
uwen_godwin <uwengodwin001@gmail.com>
2024-06-29 01:42:52 +0100

    handleResponseFromAP

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,11 @@
- import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [19, 20, 34].map(size => new ClassRoom(size));
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
 
 

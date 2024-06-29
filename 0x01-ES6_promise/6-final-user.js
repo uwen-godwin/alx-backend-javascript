2307e84fbd24930903b0110577e75cc69dee9619
uwen_godwin <uwengodwin001@gmail.com>
2024-06-29 14:20:25 +0100

    ES6 Promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,12 +6,10 @@ function handleProfileSignup(firstName, lastName, fileName) {
   const uploadPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([signUpPromise, uploadPromise])
-    .then(results =>
-      results.map(result => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }))
-    );
+    .then(results => results.map(result => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
 
 export default handleProfileSignup;

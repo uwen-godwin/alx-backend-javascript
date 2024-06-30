bac27ac4b2d7b670c2a620d867c1ba1f9d1837f9
uwen_godwin <uwengodwin001@gmail.com>
2024-06-30 15:38:55 +0100

    ES6 classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this._validateName(name);
-    this._length = this._validateLength(length);
-    this._students = this._validateStudents(students);
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {

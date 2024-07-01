f10f5efd5365d99b2d264597411d747f92c01c81
uwen_godwin <uwengodwin001@gmail.com>
2024-07-01 17:25:42 +0100

    ES6 classes

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,13 +1,15 @@
- export default class HolbertonClass {
+export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
   }
 
+  // Casting to Number
   valueOf() {
     return this._size;
   }
 
+  // Casting to String
   toString() {
     return this._location;
   }

fe2a4655d2960ac5b156c2a61b0aa001a2796a89
uwen_godwin <uwengodwin001@gmail.com>
2024-07-01 16:58:23 +0100

    ES6 classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,12 +1,33 @@
- import EVCar from './100-evcar.js';
+import EVCar from './100-evcar'; // Corrected import statement without .js extension
 
-const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
+class Car {
+    constructor(make, model, color) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+    }
+}
+
+class EVCar extends Car {
+    constructor(make, model, color, range) {
+        super(make, model, color);
+        this.range = range;
+    }
+
+    cloneCar() {
+        return new Car(this.make, this.model, this.color);
+    }
+}
+
+const ec1 = new EVCar('Tesla', 'Turbo', 'Red', '250');
 console.log(ec1);
 
 const ec2 = ec1.cloneCar();
 console.log(ec2);
 
 console.log(ec1 instanceof EVCar); // Should print: true
-console.log(ec2 instanceof Car);    // Should print: true
-console.log(ec2 instanceof EVCar);  // Should print: false
-console.log(ec1 == ec2);            // Should print: false
+console.log(ec2 instanceof Car);   // Should print: true
+console.log(ec2 instanceof EVCar); // Should print: false
+console.log(ec1 === ec2);          // Should print: false (using === for strict equality)
+
+export default EVCar; // If you want to export EVCar

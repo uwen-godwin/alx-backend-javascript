e1b0a50e4afd3ebe74458f8adb968dbd0e172fdf
uwen_godwin <uwengodwin001@gmail.com>
2024-07-01 17:50:23 +0100

    ES6 classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,16 +1,11 @@
-import Airport from './7-airport';
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+    this[Symbol.toStringTag] = 'Airport';
+  }
 
-describe('Airport', () => {
-  it('should create an Airport object', () => {
-    const airportSF = new Airport('San Francisco Airport', 'SFO');
-    expect(airportSF).toEqual(expect.objectContaining({
-      _name: 'San Francisco Airport',
-      _code: 'SFO'
-    }));
-  });
-
-  it('should return correct string description', () => {
-    const airportSF = new Airport('San Francisco Airport', 'SFO');
-    expect(airportSF.toString()).toEqual('[object SFO]');
-  });
-});
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}

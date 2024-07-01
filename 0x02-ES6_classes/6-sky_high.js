9e95ce414f50b6187a9cbcfb2db26f660cdb7439
uwen_godwin <uwengodwin001@gmail.com>
2024-07-01 17:53:03 +0100

    ES6 classes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,9 +1,13 @@
- import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
   }
 
   get floors() {

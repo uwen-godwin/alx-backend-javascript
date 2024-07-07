70d2436e55c02f1cf18b44ef9b80739fa3b880cc
uwen_godwin <uwengodwin001@gmail.com>
2024-07-07 15:51:15 +0100

    ES6 Basics

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,31 +1,32 @@
 export default function createIteratorObject(report) {
-  const employees = [];
-
-  // Iterate through each department in the report object
-  for (const department in report) {
-      if (Object.prototype.hasOwnProperty.call(report, department)) {
-          // Iterate through each employee in the current department
-          for (const employee of report[department]) {
-              employees.push(employee);
-          }
-      }
+    const employees = [];
+  
+    // Iterate through each department in the report object
+    for (const department in report) {
+        if (Object.prototype.hasOwnProperty.call(report, department)) {
+            // Iterate through each employee in the current department
+            for (const employee of report[department]) {
+                employees.push(employee);
+            }
+        }
+    }
+  
+    // Return an object with a Symbol.iterator method
+    return {
+        [Symbol.iterator]() {
+            let index = 0;
+            // Return an iterator object
+            return {
+                next() {
+                    // Check if there are more employees to iterate over
+                    if (index < employees.length) {
+                        return { value: employees[index++], done: false };
+                    } else {
+                        return { value: undefined, done: true };
+                    }
+                }
+            };
+        }
+    };
   }
-
-  // Return an object with a Symbol.iterator method
-  return {
-      [Symbol.iterator]() {
-          let index = 0;
-          // Return an iterator object
-          return {
-              next() {
-                  // Check if there are more employees to iterate over
-                  if (index < employees.length) {
-                      return { value: employees[index++], done: false };
-                  } else {
-                      return { value: undefined, done: true };
-                  }
-              }
-          };
-      }
-  };
-}
+  

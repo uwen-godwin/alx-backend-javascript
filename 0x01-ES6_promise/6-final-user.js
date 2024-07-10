e9022e65291897721f5c361010c373810b4b45f4
uwen_godwin <uwengodwin001@gmail.com>
2024-07-10 14:40:19 +0100

    ES6 Promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,4 +1,4 @@
- import signUpUser from './4-user-promise';
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
@@ -12,4 +12,3 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
         }));
     });
 }
- 

7a35dfbdf35010529266c856e1f1c125e7ce2cb5
uwen_godwin <uwengodwin001@gmail.com>
2024-07-10 14:57:56 +0100

    ES6 Promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,4 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
-/**
- * Handles profile signup by signing up a user and uploading a photo.
- * @param {string} firstName - The first name of the user.
- * @param {string} lastName - The last name of the user.
- * @param {string} fileName - The file name of the photo to upload.
- * @returns {Promise<Array>} A promise that resolves with an array of results.
- */
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Execute both promises concurrently and return the combined result.
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),

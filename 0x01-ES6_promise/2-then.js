18b88c0df3f8f11f4cbdea3a6c58147381d90742
uwen_godwin <uwengodwin001@gmail.com>
2024-07-10 15:03:51 +0100

    ES6 Promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => (
-      results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
       }))
     ));
 }

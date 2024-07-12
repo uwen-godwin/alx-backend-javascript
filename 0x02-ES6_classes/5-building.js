463192bbe27f66206f7b0f144bd4d3929465de3b
uwen_godwin <uwengodwin001@gmail.com>
2024-07-12 16:49:06 +0100

    ES6 classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -10,6 +10,7 @@ export default class Building {
     return this._sqft;
   }
 
+  // Abstract method to be overridden by subclasses
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }

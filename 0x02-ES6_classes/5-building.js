3f37e7b4b0ec54bb80fdc1b0eee0a542aae71d21
uwen_godwin <uwengodwin001@gmail.com>
2024-07-12 16:43:21 +0100

    ES6 classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,4 +1,4 @@
- export default class Building {
+export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
   }
@@ -7,6 +7,7 @@
     return this._sqft;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }

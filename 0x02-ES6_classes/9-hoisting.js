c6de197f1b02e8f4456594cde45ae7f16161419f
uwen_godwin <uwengodwin001@gmail.com>
2024-07-12 16:57:45 +0100

    ES6 classes

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,3 +1,4 @@
+// Define HolbertonClass before using it in class2019 and class2020
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -13,6 +14,7 @@ export class HolbertonClass {
   }
 }
 
+// Define StudentHolberton before using it in the list of students
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -33,13 +35,16 @@ export class StudentHolberton {
   }
 }
 
+// Create instances of HolbertonClass
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
+// Create instances of StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
+// Export list of students
 export const listOfStudents = [student1, student2, student3, student4, student5];

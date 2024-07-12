592f5ec187f9cff1ec5af51b7a76ea7099357016
uwen_godwin <uwengodwin001@gmail.com>
2024-07-12 16:33:46 +0100

    ES6 classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,23 +1,19 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = [];
-
     // Verify types during object creation
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
-
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
-
     if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
 
-    this._students = students.slice();
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   // Getter and setter for name
@@ -53,6 +49,6 @@ export default class HolbertonCourse {
     if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    this._students = newStudents.slice();
+    this._students = newStudents;
   }
 }

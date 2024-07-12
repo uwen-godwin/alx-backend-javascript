d43d8ac361d3697a58ed2069f13ae51c7f4c3f1d
uwen_godwin <uwengodwin001@gmail.com>
2024-07-12 16:50:51 +0100

    ES6 classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
     this._sqft = sqft;
+
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  // Abstract method to be overridden by subclasses
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(param) {
+    this._sqft = param;
   }
 }

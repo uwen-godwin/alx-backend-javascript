7e826ae5a7aa5dd3f6c54ea7f11bb21ed159355d
uwen_godwin <uwengodwin001@gmail.com>
2024-08-19 21:20:34 +0100

    Node js Basic

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+
+// Create an interface for input and output streams
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Display the welcome message
+console.log('Welcome to Holberton School, what is your name?');
+
+// Prompt the user for input
+rl.question('', (name) => {
+  // Output the user's name
+  console.log(`Your name is: ${name}`);
+
+  // Close the readline interface and print the closing message
+  rl.close();
+  console.log('This important software is now closing');
+});

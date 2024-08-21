7bf454a1b626beecb7308338fe8b4d4ef277d934
uwen_godwin <uwengodwin001@gmail.com>
2024-08-21 17:13:33 +0100

    Node js Basic

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8').trim();
-    const lines = data.split('\n').filter(line => line.length > 0);
+    const lines = data.split('\n').filter((line) => line.length > 0);
     const totalStudents = lines.length - 1;
 
     console.log(`Number of students: ${totalStudents}`);
